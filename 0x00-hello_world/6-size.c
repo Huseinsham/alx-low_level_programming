998cebf0ece9f2eda09b7308c5a98a57bcd2d4d2
husein sham <shamhusein3@gmail.com>
2023-02-18 17:18:53 +0000

    size of premitive data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - this is the main fuction
+*
+* Return: 0 if success
+*/
+
+int main(void)
+{
+	printf("size of a char: %lu byte(s)", sizeof(char))
+	printf("size of a int: %lu byte(s)", sizeof(int))
+	printf("size of a long int: %lu byte(s)", sizeof(long int))
+	printf("size of a long long: %lu byte(s)", sizeof(long long))
+	printf("size of a float: %lu byte(s)", sizeof(float))
+}
