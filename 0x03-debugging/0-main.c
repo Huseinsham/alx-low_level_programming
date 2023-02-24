bdf22fbe4ed9dec57358499ee24e6da71a1ff84b
husein sham <shamhusein3@gmail.com>
2023-02-24 04:32:42 +0000

    positive or nagative numbers

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
- * main - Tests function that prints positive or nagative integer
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
- *
  */
 
 int main(void)
-
 {
 	int i;
 
-	i = 98;
-
-	positive_or_nagative(i);
+	i = 0;
+	positive_or_negative(i);
 
 	return (0);
 }
--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,1 +1,8 @@
-i'm learning how to debug codes
+# **0x03. C - Debugging**
+
+## General
+
+* What is debugging
+* What are some methods of debugging manually
+* How to read the error messages
+
