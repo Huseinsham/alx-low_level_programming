c1ed292c8ad74716f332b9b25de4cedcfb082a5d
husein sham <shamhusein3@gmail.com>
2023-02-24 04:44:37 +0000

    prevent infinate loop

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,22 +1,16 @@
-#include "holberton.h" 
+#include "main.h"
 
- /** 
-  * _islower - Check Holberton 
-  * @c: An input character 
-  * Description: function uses _putchar function to print 
-  * alphabet in lowercase 10 times 
-  * Return: 1 if is lowercase or 0 if is uppercase 
-  */ 
- int _islower(int c) 
- { 
-         char i; 
-         int lower = 0; 
-  
-         for (i = 'a'; i <= 'z'; i++) 
-         { 
-                 if (i == c) 
-                         lower = 1; 
-         } 
-  
-         return (lower); 
- }
+ /**
+ * _islower - Check Holberton
+ * 
+ * Description: function uses _putchar function to print
+ * 
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
